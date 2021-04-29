d79abefd598f5dfec53f866f3e1667159bd43044
Geivan Junior <geivanjr@ufu.br>
2021-04-29 15:35:04 -0300

    change

--- teste.c
+++ teste.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main() {
-  printf("Hello world!!");
-  printf("Este código foi feito no vscode"); \\fazendo um commit aqui
+  printf("Hello world!!\n");
+  printf("Este código foi feito no vscode"); //fazendo um commit aqui
   return 0;
 }
