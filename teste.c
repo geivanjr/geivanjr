a209ed95d34ceb279b723fcc91e5af18853734ea
Geivan Junior <geivanjr@ufu.br>
2021-04-28 21:05:14 -0300

    changes

--- teste.c
+++ teste.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 int main() {
-		printf("Hello world!!");
-    printf("Esta desgraca foi feita no vscode");
+  printf("Hello world!!");
+  printf("Esta desgraca foi feita no vscode");
+  printf("Help");
     return 0;
 }
